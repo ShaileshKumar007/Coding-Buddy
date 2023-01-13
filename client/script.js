06ab036a724e172179f4b2c6b1abee311a4e92c5
Shailesh <shailesh1429k@gmail.com>
2023-01-13 22:02:14 +0530

    change the server address

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
 
   // fetching the bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://coding-buddy.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
